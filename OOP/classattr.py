017f279719b281bec8a066578d97ea772a21d4e2
Semiu <ayobami.sm@gmail.com>
2021-01-13 16:01:25 -0600

    attributes completed

--- OOP/classattr.py
+++ OOP/classattr.py
@@ -12,15 +12,15 @@ student1 = Students("Semiu Akanmu", 37, 700)
 print (student1.name) #Semiu Akanmu
 print (student1.age) #37
 print (student1.level) #700
-"""
+
 try:
     print (student1.hobby)
 except:
     print ("Error because hobby was not initialized as an attribute for Students class")
     #Error because hobby was not initialized as an attribute for Students class
     #The try and except bloc is to handle the error that would be generated
-    #in the try bloc. This is commented out to allow follow up code to run.
-"""
+    #in the try bloc. 
+
 #Checking if an object of a class has an attribute
 student2 = Students("Habeeb Akanmu", 12, 50)
 print(hasattr(student2, "age")) #True
