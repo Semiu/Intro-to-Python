eb6a10dc35cb70b02f707647648d6abea6e50a8a
Semiu Akanmu <ayobami.sm@gmail.com>
2021-01-16 19:48:44 -0600

    Update printToScreen.py

--- Basics/printToScreen.py
+++ Basics/printToScreen.py
@@ -1,6 +1,6 @@
 #A simple Python program that prints “Please enter an integer:” to the screen
 def printToScreen ():
-  print (“Please enter an integer:”)
+  print ("Please enter an integer:")
 
 #The printToScreen function is called
 printToScreen()
