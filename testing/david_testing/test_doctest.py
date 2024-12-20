eba06aca4274b634e8bc14d3ee1d94d73d294a45
Semiu Akanmu <ayobami.sm@gmail.com>
2024-12-20 16:36:18 -0600

    experimented docttest using David Sales book

---
+++ testing/david_testing/test_doctest.py
@@ -0,0 +1,19 @@
+class Calculator:
+    def __init__(self):
+        pass
+    def add_int(self, x, y):
+        """
+        add two integers and produce the result
+        >>> c = Calculator()
+        >>> c.add_int(1,1)
+        2
+
+        >>> c.add_int(12, 15)
+        27
+        """
+        return x + y
+    
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
